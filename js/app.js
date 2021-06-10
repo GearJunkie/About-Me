45da4420c6ef9c57755946029355ab4dd210c6ea
jesse dills <jessedills@yahoo.com>
2021-06-09 22:14:26 -0400

    added js scorekeeper alert

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict'
 
+let guessScore = 0;
+// let guessScore;
 let username = prompt("Hello, could you plase tell me your name?");
 //line 3 is called a statement and statements always end with a semicolon. username is the object. = is an assignement operator. prompt() is a method. The content within the () is called the parameters.
 
@@ -19,6 +21,7 @@ console.log(backpacking + lowercaseBackpacking);
 
 if (lowercaseBackpacking === "yes" || lowercaseBackpacking === "y") {
   alert("Isn't it such a rewarding experience?")
+  guessScore++
 } else if (lowercaseBackpacking === "no" || lowercaseBackpacking === "n") {
   alert("You're missing out on a truly wonderful experience!");
 } else {
@@ -32,6 +35,7 @@ console.log(packrafting + lowercasePackrafting);
 
 if (lowercasePackrafting === "yes" || lowercasePackrafting === "y") {
   alert("This takes backpacking to a whole new level!");
+  guessScore++
 } else if (lowercasePackrafting === "no" || lowercasePackrafting === "n") {
   alert("Oh, but you should! Packrafting allows you to reach places that backpacking could never take you.");
 } else {
@@ -44,45 +48,49 @@ console.log(floaternighter + lowercaseFloaternighter);
 
 if (lowercaseFloaternighter === "yes" || lowercaseFloaternighter === "y") {
   alert("These are one of my favorite ways to go camping!");
+  guessScore++
 } else if (lowercaseFloaternighter === "no" || lowercaseFloaternighter === "n") {
     alert("A 'floaternighter' is a super fun overnight camp trip that uses watercraft to reach your campsite.");
-  } else {
+} else {
     alert("Hmm, you are really struggling to stay on topic!");
-  }
+}
 
-  let gearjunkie = prompt("Would you consider yourself a gear junkie (someone that collects lots of items that are related to their hobbies)?");
-  let lowercaseGearJunkie = gearjunkie.toLowerCase();
+let gearjunkie = prompt("Would you consider yourself a gear junkie (someone that collects lots of items that are related to their hobbies)?");
+let lowercaseGearJunkie = gearjunkie.toLowerCase();
   console.log(gearjunkie + lowercaseGearJunkie);
 
-  if (lowercaseGearJunkie === "yes" || lowercaseGearJunkie === "y") {
+if (lowercaseGearJunkie === "yes" || lowercaseGearJunkie === "y") {
     alert("Me too! I am always looking for cool gear to add to my collection!");
-  } else if (lowercaseGearJunkie === "no" || lowercaseGearJunkie === "n") {
+    guessScore++
+} else if (lowercaseGearJunkie === "no" || lowercaseGearJunkie === "n") {
     alert("Being a gear junkie is not for everyone!");
-  } else {
+} else {
     alert("I've had about enough of this!");
-  }
+}
 
-  let outdoorenthusiast = prompt("Do you consider yourself an outdoor enthusiast?");
-  let lowercaseOutdoorEnthusiast = outdoorenthusiast.toLowerCase();
+let outdoorenthusiast = prompt("Do you consider yourself an outdoor enthusiast?");
+let lowercaseOutdoorEnthusiast = outdoorenthusiast.toLowerCase();
   console.log(outdoorenthusiast + lowercaseOutdoorEnthusiast);
 
-  if (lowercaseOutdoorEnthusiast === "yes" || lowercaseOutdoorEnthusiast === "y") {
+if (lowercaseOutdoorEnthusiast === "yes" || lowercaseOutdoorEnthusiast === "y") {
     alert(username + ", most of our daily lives are centered around the developed world, so I think that it's good to get outdoors as often as possible.");
-  } else if (lowercaseOutdoorEnthusiast === "no" || lowercaseOutdoorEnthusiast === "n") {
+    guessScore++
+} else if (lowercaseOutdoorEnthusiast === "no" || lowercaseOutdoorEnthusiast === "n") {
     alert(username + ", you might be surprised how much you would enjoy giving any remote outdoor activity a try!");
-  } else {
+} else {
     alert(username + ", please leave my webpage!");
-  }
+}
 
-  let correctAnswer = 39
-  let userAttempts = 4
-  for (let i = 0; i < userAttempts; i++){
-    let userGuess = prompt("Can you guess my age? Choose a number between 1-50");
+let correctAnswer = 39
+let userAttempts = 4
+for (let i = 0; i < userAttempts; i++){
+  let userGuess = prompt("Can you guess my age? Choose a number between 1-50");
     while (userGuess < 1 || userGuess > 50) {
         userGuess = prompt("Not within the Range. Please enter a number between 1 and 50");
     }
     if (userGuess == correctAnswer) {
         alert("Yes, I'm 39 years old!");
+        guessScore++
         break;
     } else if (userGuess < correctAnswer) {
         alert("too low, try again")
@@ -95,25 +103,30 @@ if (lowercaseFloaternighter === "yes" || lowercaseFloaternighter === "y") {
     if (i == userAttempts -1) {
         alert("You did not guess my correct age. The correct answer is: " + correctAnswer);
     }
-    }
+}
 
-  let cuisineArray = ["peruvian", "mediterranean", "korean"];
-
-  let guessAttempts = 6;
-  let correctAnswer = false;
+let cuisineArray = ["peruvian", "mediterranean", "korean"];
+let guessAttempts = 6;
+let theRightAnswer = false;
   
-  while (correctAnswer === false && guessAttempts > 0) {
+  while (theRightAnswer === false && guessAttempts > 0) {
     let userGuess = prompt("Name one of my favorite types of cuisine!");
     for (let index = 0; index < cuisineArray.length; index++) {
       if (userGuess === cuisineArray[index]) {
       alert("Yes, you got it! My favorite cuisines are Peruvian, Mediterranean, and Korean.");
-      correctAnswer = true;
+      guessScore++
+      theRightAnswer = true;
       break;
+      }
     }
-    }
-    if (correctAnswer === false) {
+      if (theRightAnswer === false && guessAttempts > 1) {
       alert("Sorry, but that is not one of my favorites. Please try again.");
     }
+      if (theRightAnswer === false && guessAttempts == 1) {
+        alert("You did not answer correctly. The answers were; Peruvian, Mediterranean, and Korean.")
+        theRightAnswer = true;
+        break;
+      }
     guessAttempts--
   }
-  console.log(guessAttempts)
+  alert("Yay! You got " + guessScore + " of 7 questions correct!");
